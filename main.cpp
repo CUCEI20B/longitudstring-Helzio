25863b4f950ac5f9aba893141cd668ebb0e791cf
Oscar Mendez <oscar.mendez5121@gmai.com>
2020-09-29 09:44:41 -0500

    Pueba 1

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string input;
+    cin >> input;
+
+    cout << input.length() << endl;
 
     return 0;
 }
